c121de5245fb8fcd02fa038db2ddcbe4b9451630
erictsech <73739759+erictsech@users.noreply.github.com>
2024-03-12 12:13:31 +0800

    Update index.tsx

--- pages/App/index.tsx
+++ pages/App/index.tsx
@@ -1,31 +1,15 @@
 'use client'
 import { useState, useEffect } from 'react';
-import { bitable } from '@base-open/connector-api';
 
 export default function App() {
     const [value, setValue] = useState('');
     const [userId, setUserId] = useState('');
     const [tenantKey, setTenantKey] = useState('');
-    useEffect(() => {
-        bitable.getConfig().then(config => {
-            console.log('srcTablePath client', config);
-            setValue(config?.value || '');
-        });
-        bitable.getUserId().then(id => {
-            setUserId(id);
-        });
-        bitable.getTenantKey().then(key => {
-            setTenantKey(key);
-        })
-    }, [])
     return (
         <div >
             <div>userId:{userId}</div>
             <div>tenantKey:{tenantKey}</div>
             <input value={value} onChange={(e) => { setValue(e.target.value) }} />
-            <button onClick={() => {
-                bitable.saveConfigAndGoNext({ value, key: userId })
-            }}>下一步</button>
         </div>
     )
 }
